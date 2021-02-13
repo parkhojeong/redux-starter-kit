168399a46aed6b7000eb37cdaa3c069391037038
parkhojeong <parkhj062@gmail.com>
2021-02-14 00:52:18 +0900

    redux-pender cancelRequest

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import * as counterActions from "./modules/counter";
 import * as postActions from "./modules/post";
 
 class App extends Component {
+  cancelRequest = null;
+
+  handleCancel = () => {
+    if (this.cancelRequest) {
+      this.cancelRequest();
+      this.cancelRequest = null;
+    }
+  };
+
   loadData = async () => {
     const { PostActions, number } = this.props;
 
     try {
-      const response = await PostActions.getPost(number);
+      const p = PostActions.getPost(number);
+      this.cancelRequest = p.cancel;
+      const response = await p;
       console.log(response);
     } catch (e) {
       console.log(e);
@@ -20,6 +31,11 @@ class App extends Component {
 
   componentDidMount() {
     this.loadData();
+    window.addEventListener("keyup", (e) => {
+      if (e.key === "Escape") {
+        this.handleCancel();
+      }
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {

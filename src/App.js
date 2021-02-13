d260d9571ea9b46460288eac18efb2379b4a42a1
parkhojeong <parkhj062@gmail.com>
2021-02-13 23:36:54 +0900

    async action test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as counterActions from './modules/counter';
-
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as counterActions from "./modules/counter";
+import axios from "axios";
 
 class App extends Component {
-    render() {
-        const { CounterActions, number } = this.props;
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => console.log(response));
+  }
 
-        
-        return (
-            <div>
-                <h1>{number}</h1>
-                <button onClick={CounterActions.increment}>+</button>
-                <button onClick={CounterActions.decrement}>-</button>
-            </div>
-        );
-    }
+  render() {
+    const { CounterActions, number } = this.props;
+
+    return (
+      <div>
+        <h1>{number}</h1>
+        <button onClick={CounterActions.incrementAsync}>+</button>
+        <button onClick={CounterActions.decrement}>-</button>
+      </div>
+    );
+  }
 }
 
 export default connect(
-    (state) => ({
-        number: state.counter
-    }),
-    (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
-    })
+  (state) => ({
+    number: state.counter,
+  }),
+  (dispatch) => ({
+    CounterActions: bindActionCreators(counterActions, dispatch),
+  })
 )(App);

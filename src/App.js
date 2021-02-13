deb59c76724a0d33a26ef8f05bd94eff7762f8c9
parkhojeong <parkhj062@gmail.com>
2021-02-14 00:07:16 +0900

    redux-thunk

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as counterActions from "./modules/counter";
-import axios from "axios";
+import post, * as postActions from "./modules/post";
 
 class App extends Component {
+  loadData = async () => {
+    const { PostActions, number } = this.props;
+
+    try {
+      const response = await PostActions.getPost(number);
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => console.log(response));
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.number !== prevProps.number) {
+      this.loadData();
+    }
   }
 
   render() {
-    const { CounterActions, number } = this.props;
+    const { CounterActions, number, post, error, loading } = this.props;
 
     return (
       <div>
         <h1>{number}</h1>
-        <button onClick={CounterActions.incrementAsync}>+</button>
+        {(() => {
+          if (loading) return <h2>로딩 중..</h2>;
+
+          if (error) return <h2>에러 발생!</h2>;
+
+          return (
+            <div>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          );
+        })()}
+
+        <button onClick={CounterActions.increment}>+</button>
         <button onClick={CounterActions.decrement}>-</button>
       </div>
     );
@@ -27,8 +55,12 @@ class App extends Component {
 export default connect(
   (state) => ({
     number: state.counter,
+    post: state.post.data,
+    loading: state.post.pending,
+    error: state.post.error,
   }),
   (dispatch) => ({
     CounterActions: bindActionCreators(counterActions, dispatch),
+    PostActions: bindActionCreators(postActions, dispatch),
   })
 )(App);
